28cd67b4aef50ee20086adc980131318d207e481
Norio Ikedo <ikedo@ux-xu.com>
2019-07-19 17:37:40 +0900

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '5431a23f-e047-4365-83e8-e74ab4a5cc47'; // LED, Button
+const USER_SERVICE_UUID         = '3664aa6c-9b52-45f6-9208-bd9546724ea6'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
